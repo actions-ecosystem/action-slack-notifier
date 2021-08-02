3bd39478c7efa1d5f0125179b9b6d72a5f364555
aximov <d@alumni.tus.ac.jp>
2021-08-02 20:11:21 +0900

    add a test case

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,6 +1,6 @@
 import { createPostMessageArguments } from '../src/main';
 
-test('createPostMessageArguments colored=true, verbose=true', async () => {
+test('createPostMessageArguments colored=true, verbose=true, unfurl=true', async () => {
   const args = await createPostMessageArguments(
     'develop',
     'hello',
@@ -21,6 +21,46 @@ test('createPostMessageArguments colored=true, verbose=true', async () => {
   expect(args.blocks).toBeUndefined();
   expect(args.unfurl_links).toEqual(true);
   expect(args.unfurl_media).toEqual(true);
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  expect(args.attachments!).toEqual([
+    {
+      color: '#000000',
+      blocks: [
+        {
+          type: 'section',
+          fields: [
+            {
+              type: 'mrkdwn',
+              text: 'text'
+            }
+          ]
+        }
+      ]
+    }
+  ]);
+});
+
+test('createPostMessageArguments colored=true, verbose=true, unfurl=false', async () => {
+  const args = await createPostMessageArguments(
+    'develop',
+    'hello',
+    'Bot',
+    [
+      {
+        type: 'mrkdwn',
+        text: 'text'
+      }
+    ],
+    true,
+    '#000000',
+    false
+  );
+
+  expect(args.text).toEqual('hello');
+  expect(args.attachments).not.toBeUndefined();
+  expect(args.blocks).toBeUndefined();
+  expect(args.unfurl_links).toEqual(false);
+  expect(args.unfurl_media).toEqual(false);
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   expect(args.attachments!).toEqual([
     {
